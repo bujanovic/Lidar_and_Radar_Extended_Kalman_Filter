53efe9749e29bbf591de45702d1b38ab9039da26
bujanovic <pbujanov@gmail.com>
2019-06-18 16:53:19 -0400

    Complete Extended Kalman Filter - Version 1

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,7 +1,10 @@
+#include <math.h>
+#include <iostream>
 #include "kalman_filter.h"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using namespace std;
 
 /* 
  * Please note that the Eigen library does not initialize 
@@ -24,18 +27,63 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 
 void KalmanFilter::Predict() {
   /**
-   * TODO: predict the state
+   * Predict the state
    */
+   x_ = F_ * x_;
+   
+   P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
-   * TODO: update the state by using Kalman Filter equations
+   * Update the state with Lidar by using Kalman Filter equations
    */
+   
+   VectorXd y_k = z - H_ * x_;
+   MatrixXd S_k = H_ * P_ * H_.transpose() + R_;
+   MatrixXd K_k = P_ * H_.transpose() * S_k.inverse();
+   x_ = x_ + K_k * y_k;
+   MatrixXd I = MatrixXd::Identity(x_.size(), x_.size());
+   P_ = (I - K_k*H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
-   * TODO: update the state by using Extended Kalman Filter equations
+   * Update the state with Radar by using Extended Kalman Filter equations
    */
+   VectorXd H_x = VectorXd(3);
+   float px, py, vx, vy, rho, phi, rho_dot;
+   px = x_[0];
+   py = x_[1];
+   vx = x_[2];
+   vy = x_[3];
+   rho = sqrt(px*px + py*py);
+   phi = atan2(py, px);  // returns values between -pi and pi
+
+   //Make sure we're not dividing by zero
+   if (rho < 0.000001){
+	rho = 0.000001;
+   }
+   rho_dot = (px*vx+py*vy)/rho;
+   H_x  << rho, phi, rho_dot;
+   
+   VectorXd y_k = z - H_x;
+
+   // normalize the angle between -pi to pi
+ 
+   while(y_k(1) > M_PI){
+    y_k(1) -= 2*M_PI;
+   }
+
+   while(y_k(1) < -M_PI){
+    y_k(1) += 2*M_PI;
+   }
+   cout<<y_k(1)<<endl;
+     
+   MatrixXd S_k = H_ * P_ * H_.transpose() + R_;
+   MatrixXd K_k = P_ * H_.transpose() * S_k.inverse();
+   x_ = x_ + K_k * y_k;
+   MatrixXd I = MatrixXd::Identity(x_.size(), x_.size());
+   P_ = (I - K_k*H_) * P_;
+   
 }
